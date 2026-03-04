62beef58302816e8fb90b437cb355466268a1b35
Niba-ai-dev <nibabutt5@gmail.com>
2026-03-05 01:27:38 +0500

    Add total+ Average variable practice

---
+++ Total_average.py
@@ -0,0 +1,8 @@
+# create 3 variablesand print the total and average:
+a=100
+b=12
+c=345
+total=a+b+c
+average=(total/3) 
+print("the total and average of this :", "total is:",total, "" ,"average is:",average)
+# if yoe want that your answer in 2 decimal points then you coose round(average,2)
