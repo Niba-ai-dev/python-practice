a3624450cf4ad80b61dadf21d2dff3703c4e7d97
Niba-ai-dev <nibabutt5@gmail.com>
2026-03-05 02:15:33 +0500

    Add multiple operations(Total,average,percentage)

---
+++ students_marks.py
@@ -0,0 +1,21 @@
+'''Q#Aapke paas ek student ke 4 subjects ke marks diye gaye hain:
+Maths = 75
+Science = 82
+English = 91
+Urdu = 68
+Tasks:
+4 subjects ka total marks calculate karo.
+4 subjects ka average marks calculate karo.
+4 subjects ka percentage calculate karo (Maximum marks for each subject = 100).'''
+Maths=75
+Science=82
+English=91
+Urdu=68
+Total= Maths+Science+English+Urdu
+Average=(Maths+Science+English+Urdu/4)
+Percentage=(Maths+Science+English+Urdu/4) * 100
+print("The total is:", Total)
+print("The Average is:", Average)
+print("The Percentage is:", Percentage)
+
+
