1afa6475a8b4423a89e134d1cdaa82bb4621711f
Niba-ai-dev <nibabutt5@gmail.com>
2026-03-05 02:33:53 +0500

    Add small shop items example practice

---
+++ small_shop_items.py
@@ -0,0 +1,17 @@
+Pen_Price=15
+Pen_Quantity=10
+Total_Pen=Pen_Price*Pen_Quantity
+Notebook_Price=50
+Notebook_Quantity=5
+Total_Notebook=Notebook_Price*Notebook_Quantity
+Eraser_Price=5
+Eraser_Quantity=20
+Total_Eraser=Eraser_Price*Eraser_Quantity
+GrandTotal= Total_Pen+Total_Notebook+Total_Eraser
+Average_per_item=(GrandTotal/3)
+print("Total for Pen:",Total_Pen)
+print("Total for Notebook:",Total_Notebook)
+print("Total for Eraser:",Total_Eraser)
+print("GrandTotal  is:",GrandTotal)
+print("Average per item:",Average_per_item)
+
