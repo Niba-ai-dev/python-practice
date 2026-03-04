e349668027d52dfde4c979bbebcfac69eea91d77
Niba-ai-dev <nibabutt5@gmail.com>
2026-03-05 01:14:59 +0500

    Add Percentage between two variables practice

---
+++ percentage_calculate.py
@@ -0,0 +1,6 @@
+# Calculate the Percentage of a variable:
+numbers= 230
+Total_numbers=600
+Percentage= (numbers/Total_numbers ) * 100
+
+print("The Percentage of numbers and Total_numbers  is: --->" , Percentage)
