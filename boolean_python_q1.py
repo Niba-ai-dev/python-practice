6298a62d3ca987e2dc30084a925a6ae598047dd1
Niba-ai-dev <nibabutt5@gmail.com>
2026-03-09 13:00:31 +0500

    Add python boolean practice

---
+++ boolean_python_q1.py
@@ -0,0 +1,7 @@
+# Qno1: Write a code to check ifa number is positive or not with boolean(True ore False)?
+num=input("Enter a number:")
+num=int(num)
+if num >0 :
+    print(True)
+else:
+    print(False)
