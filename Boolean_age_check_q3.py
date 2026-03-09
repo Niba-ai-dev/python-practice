ea1789f325ed8dd254ab142eed4a7bd476f51fcb
Niba-ai-dev <nibabutt5@gmail.com>
2026-03-09 13:08:45 +0500

    Add boolean check age

---
+++ Boolean_age_check_q3.py
@@ -0,0 +1,6 @@
+# Qno3: Wrie a code to check if a input age is greater ore equal to 18 then they give the vote ore not>
+age=int(input("Enter a ager number:"))
+if age >=18:
+    print(True)
+else:
+    print(False)        
